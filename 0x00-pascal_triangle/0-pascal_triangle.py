c6a3ba74cdc9587dedadb37b1e5c1eab5d8278cf
root <root@localhost.localdomain>
2024-04-25 12:10:07 +0100

    Pascal's triangle function implemented

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""define the pascal_triangle() function"""
+
+
 def pascal_triangle(n):
     """returns a list of lists of integers
        representing the Pascal’s triangle of n
