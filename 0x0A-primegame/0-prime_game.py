5389a8f240b5001de90003f15e54aaf6fdde0b69
ChrysArex <chrysazokly@gmail.com>
2024-09-08 23:46:16 +0100

    build the function to get the winner

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def isWinner(x, nums):
+    score = {"Maria": 0, "Ben": 0}
+    for n in nums:
+        numbers = list(range(1, n + 1))
+        rocket = ["Maria", "Ben", ""]
+        if len(numbers) > 1:
+            while (len(numbers) != 1):
+                target = numbers[1]
+                for i in numbers[1:]:
+                    if i % target == 0:
+                        numbers.remove(i)
+                rocket[2] = rocket[0]
+                rocket[0] = rocket[1]
+                rocket[2] = rocket[1]
+            score[rocket[1]] += 1
+        else:
+            score["Ben"] += 1
+
+    if score["Maria"] > score["Ben"]:
+        return "Maria"
+    elif score["Maria"] == score["Ben"]:
+        return None
+    else:
+        return "Ben"
